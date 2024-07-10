4a0a3f0d2b5dfdfd7e8478b2dda219fd09b5020c
Toan Nguyen Truong <toannguyentruong1309@gmail.com>
2024-07-10 09:32:51 +0700

    Initial commit

---
+++ src/main/java/org/example/springbootstart/SpringBootStartApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootstart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStartApplication.class, args);
+    }
+
+}
